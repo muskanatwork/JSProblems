66b00a7f73a648a186b1ed6b1b552ef44520acd4
unknown <kumari.muskan2302@gmail.com>
2023-09-05 20:38:57 +0530

    Make some changes

---
+++ repeatString.js
@@ -0,0 +1,9 @@
+function sum(num) {
+    var answer =num
+        for (var i = 0; i < num; i++) {
+            answer = answer + num
+        }
+        console.log(answer)
+}
+sum(4);
+            
